e2075aef45dd4f594b9036fc83fbdc6a3886dc2b
michaelqmaguire <67400297+michaelqmaguire@users.noreply.github.com>
2022-01-24 10:41:19 -0500

    initial commit

--- 01_171_nested-data-and-nested-iteration.py
+++ 01_171_nested-data-and-nested-iteration.py
@@ -1,4 +1,3 @@
-# %%
 # Nested Data and Nested Iteration
 
 # ------------------------------------------------------------- #
@@ -21,8 +20,6 @@ data = ['bagel', 'cream cheese', 'breakfast', 'grits', 'eggs', 'bacon', [34, 9, 
 plant = data[7][0][0]
 
 print(plant)
-# %%
-
 # ------------------------------------------------------------- #
 # 17.1.1 - Nested Dictionaries - Check Your Understanding       #
 # ------------------------------------------------------------- #
@@ -48,8 +45,30 @@ color = info['personal_data']['physical_features']['color']
 
 print(color)
 
-# %%
 # ------------------------------------------------------------- #
-# 17.3 - Nested Dictionaries - Check Your Understanding       #
+# 17.4 - Nested Iteration - Check Your Understanding            #
 # ------------------------------------------------------------- #
 
+#2. Below, we have provided a list of lists that contain information about people. Write code to create a new list that contains every person’s last name, and save that list as last_names.
+
+info = [['Tina', 'Turner', 1939, 'singer'], ['Matt', 'Damon', 1970, 'actor'], ['Kristen', 'Wiig', 1973, 'comedian'], ['Michael', 'Phelps', 1985, 'swimmer'], ['Barack', 'Obama', 1961, 'president']]
+
+last_names = []
+
+for person in info:
+  last_names.append(person[1])
+
+print(last_names)
+
+# 3. Below, we have provided a list of lists named L. Use nested iteration to save every string containing “b” into a new list named b_strings.
+
+L = [['apples', 'bananas', 'oranges', 'blueberries', 'lemons'], ['carrots', 'peas', 'cucumbers', 'green beans'], ['root beer', 'smoothies', 'cranberry juice']]
+
+b_strings = []
+
+for lst in L:
+  for sublst in lst:
+    if 'b' in sublst:
+      b_strings.append(sublst)
+
+print(b_strings)
